9e7474475a339bbf82f14c1d7113cf7375dc4170
Fabian Herzog <fabianfubel@gmail.com>
2015-12-11 00:25:41 +0100

    changes

--- lepyrello.py
+++ lepyrello.py
@@ -9,8 +9,7 @@
 
 	TODO:
 			*	Optimize code: This is the first commit. Code could be faster.
-			*	Deal with Exceptions: If some files in the directory are no 
-									  music files or have no ID3 frame...
+			*	Deal with Exceptions
 			*	Build a GUI: To make this useful to a normal user
 			*	Generalize the code to sort by other ID3 tags in similar way 
 			*	Modulize this 
@@ -20,65 +19,87 @@
 import os
 import pathlib
 import operator
+import argparse 
 
 from collections import OrderedDict
 
 # Third party imports
 from mutagen.id3 import ID3, TPOS, TRCK
 
+
 def get_audio_tpos(audio):
-	return str(audio.get('TPOS')).split('/')[0]
+    return str(audio.get('TPOS')).split('/')[0]
+
 
 def get_audio_track(audio):
-	return str(audio.get('TRCK')).split('/')[0]
+    return str(audio.get('TRCK')).split('/')[0]
+
 
 def set_id3_track(audio, nr):
-	nr = str(nr)
-	nr = nr + "/" + nr
-	audio.add(TRCK(encoding=1, text=nr))
-	audio.save()
+    nr = str(nr)
+    nr = nr + "/" + nr
+    audio.add(TRCK(encoding=1, text=nr))
+    audio.save()
+
 
 def generate_movement_dict(audios):
-	mvmts = OrderedDict()
-	files_in_dir = [str(p) for p in pathlib.Path(audios).iterdir() if p.is_file()]
-	for audio in files_in_dir:
-		audio_tpos = get_audio_tpos(ID3(audio))
-		audio_track = int(get_audio_track(ID3(audio)))
-		try:
-			mvmts[audio_tpos].append((audio, audio_track))
-		except KeyError:
-			mvmts[audio_tpos] = [(audio, audio_track)]
-	for k in mvmts:
-		mvmts[k].sort(key=operator.itemgetter(1))
+    mvmts = OrderedDict()
+    files_in_dir = [str(p)
+                    for p in pathlib.Path(audios).iterdir() if p.is_file()]
+    for audio in files_in_dir:
+        audio_tpos = get_audio_tpos(ID3(audio))
+        audio_track = int(get_audio_track(ID3(audio)))
+        try:
+            mvmts[audio_tpos].append((audio, audio_track))
+        except KeyError:
+            mvmts[audio_tpos] = [(audio, audio_track)]
+    for k in mvmts:
+        mvmts[k].sort(key=operator.itemgetter(1))
 
-	return mvmts
+    return mvmts
+
 
 def number_audios(dictionary):
-	j = 1
-	keylist = []
-	for key in dictionary:
-		for audio in dictionary[key]:
-			audio = audio[0]
-			path, filename = os.path.split(audio)
-			audio_new = os.path.join(path, str(j) + ' ' + filename)
-			os.rename(audio, audio_new)
-			set_id3_track(ID3(audio_new), j)
-			j = j+1
-	return j
+    j = 1
+    keylist = []
+    for key in dictionary:
+        for audio in dictionary[key]:
+            audio = audio[0]
+            path, filename = os.path.split(audio)
+            audio_new = os.path.join(path, str(j) + ' ' + filename)
+            os.rename(audio, audio_new)
+            set_id3_track(ID3(audio_new), j)
+            j = j+1
+    return j
+
 
 def update_id3_max_track(audios, max_track):
-	files_in_dir = [str(p) for p in pathlib.Path(audios).iterdir() if p.is_file()]
-	for audio in files_in_dir:
-		audio = ID3(audio)
-		set_id3_track(audio, max_track)
+    files_in_dir = [str(p)
+                    for p in pathlib.Path(audios).iterdir() if p.is_file()]
+    for audio in files_in_dir:
+        audio = ID3(audio)
+        set_id3_track(audio, max_track)
+
 
 def movements_to_track(directory):
-	movements = generate_movement_dict(directory)
-	print(movements)
-	max_track = number_audios(movements)
-	# update_id3_max_track(directory, max_track)
-	print("finished.")
+    movements = generate_movement_dict(directory)
+    max_track = number_audios(movements)
+    # update_id3_max_track(directory, max_track)
+    print("finished.")
+
+
+def main():
+    parser = argparse.ArgumentParser(description='converts audio files with id3 movement and\
+                                     track tag to audio files with id3 track tag only')
+    parser.add_argument('-d', dest='directory', help='directory containing the files')
+    args = parser.parse_args()
+
+    if not args.directory:
+    	parser.print_help()
+    	parser.exit(1)
+    
+    movements_to_track(args.directory)
 
 
 if __name__ == "__main__":
-    movements_to_track("/home/fabianfubel/nfblm/")
+    main()
